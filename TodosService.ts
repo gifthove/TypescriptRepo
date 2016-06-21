f87df5f3a7ebabf3573b30f73b574169a11bc3c8
gift.hove@britehouse.co.za <gift.hove@britehouse.co.za>
2016-06-21 16:08:58 +0200

    Created the TodoService.ts file with the TodoService

---
+++ TodosService.ts
@@ -0,0 +1,19 @@
+/**
+ * name
+ */
+class TodosService {
+    
+    static __id : number = 0;
+    
+    todos : any[];
+    
+    constructor() {
+        
+        this.todos =[
+            {id: TodosService.__id++, name: 'Clean Cave', completed:false},
+            {id: TodosService.__id++, name: 'Dryclean cape', completed:false},
+            {id: TodosService.__id++, name: 'Save Gotham', completed:false},
+        ];
+        
+    }
+}
